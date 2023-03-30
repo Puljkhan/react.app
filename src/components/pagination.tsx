1b777ea288f0c0f6e2e96c61eea6e53b6b7eddf8
grafika10 <grafika10@ucione.local>
2023-03-30 20:29:43 +0200

    Edit2

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,9 +1,18 @@
 import iconLeft from "./../assets/icon-left.svg";
+import iconRight from "./../assets/icon-right.svg";
+
 const Pagination = () => {
   return (
-    <div>
-      <div>
-        <img src={iconLeft} alt="" />
+    <div className="pagination">
+      <div className="pagination__arrow">
+        <img src={iconLeft} alt="previous page" />
+      </div>
+      <div className="pagination__item">1</div>
+      <div className="pagination__item">2</div>
+      <div className="pagination__item">3</div>
+      <div className="pagination__item">4</div>
+      <div className="pagination__arrow">
+        <img src={iconRight} alt="next page" />
       </div>
     </div>
   );
--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -1,8 +1,20 @@
-import creativeIcon from "./../assets/icon-creative.svg";
+import IconLightbulb from "./../assets/icon-lightbulb.svg";
+
 const Tags = () => {
   return (
     <div>
-      <div className="tag"></div>
+      <div className="tag">
+        <span className="tag__label">Tag 1</span>
+        <IconLightbulb />
+      </div>
+      <div className="tag">
+        <span className="tag__label">Tag 2</span>
+        <IconLightbulb />
+      </div>
+      <div className="tag">
+        <span className="tag__label">Tag 3</span>
+        <IconLightbulb />
+      </div>
     </div>
   );
 };
