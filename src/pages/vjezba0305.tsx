fc024ee96abae316235c26a4c3eca95a7262e174
grafika10 <grafika10@ucione.local>
2023-05-05 21:38:56 +0200

    add vjezba0305 new

--- src/pages/vjezba0305.tsx
+++ src/pages/vjezba0305.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 const Vjezba0305 = () => {
   //1. dohvatimo box i promjenimo mu background-color (kroz JS)
   //2. dodamo dodatnu klasu na box koja će povecati njegove dimenzije
@@ -7,39 +5,25 @@ const Vjezba0305 = () => {
   //3.2. animirati box dijagonalno do (30px,30px) i nazad do 50px
   //4. kreiramo funkciju koja će imati lokalnu varijablu u kojoj ćemo izracunati sumu 2 parametra i rezultat ispisati unuatr box-a
   //5. dodaj onClick event i na klik smanji dimenzije box-a
-  let position = 0;
-  let reverse = false;
-  useEffect(() => {
-    const box = document.getElementsByClassName(`box`)[0] as HTMLDivElement;
-    const frame = () => {
-      const currentPosition = position + `px`;
 
-      if (reverse) {
-        position--;
-      } else {
-        position++;
-      }
-      if (position == 30) {
-        reverse = true;
-      }
-      if (position == 0) {
-        reverse = false;
-      }
+  const handleClick = () => {
+    let box = document.getElementsByClassName("box")[0] as HTMLDivElement;
+    box.style.transform = "scale(0.2)";
+  };
 
-      box.style.marginLeft = currentPosition;
-    };
-    setInterval(frame, 30);
-  }, []);
+  const sum = (x: number, y: number) => {
+    return x + y;
+  };
 
-  const handleClick = () => {};
+  const box = document.getElementsByClassName("box")[0] as HTMLDivElement;
+  box.style.backgroundColor = `red`;
 
   return (
     <div className="container">
-      <h1>Vjezba0305</h1>
+      <h1>Vježba 0305</h1>
       <hr />
-      <div className="box"></div>
+      <div className="box">{sum(2, 2)}</div>
       <button onClick={() => handleClick()}>Click me</button>
-      <button value="red">Change color</button>
     </div>
   );
 };
