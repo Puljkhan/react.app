e457c35e988cd7dc74eab92d03d8ef6006f3524d
Ivan Puljić <IvanPuljich955@gmail.com>
2023-05-10 17:47:36 +0200

    add new pokemon

--- src/pages/pokemons.tsx
+++ src/pages/pokemons.tsx
@@ -1,66 +1,120 @@
 import { useEffect, useState } from "react";
 
-type BerryType = {
+type PokemonType = {
+  name: string;
+  id: number;
   url: string;
+};
+
+type PokemonAbilityNames = {
   name: string;
 };
 
+type PokemonAbilityType = {
+  name: string;
+  id: number;
+  names: PokemonAbilityNames[];
+};
+
+type BerryType = {
+  name: string;
+  url: string;
+};
+
 const Pokemons = () => {
-  const getCapitalCity = () => {
-    fetch("https://restcountries.com/v3.1/capital/tallinn")
+  const [pokemonData, setPokemonData] = useState<PokemonType>();
+  const [pokemonAbility, setPokemonAbility] = useState<PokemonAbilityType>();
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [berryData, setBerryData] = useState<BerryType[]>([]);
+  const [flag, setFlag] = useState<string>("");
+
+  const getPokemonAbility = (id: number) => {
+    fetch(`https://pokeapi.co/api/v2/ability/${id}`)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        console.log(data.results);
-        setBerries(data.results);
+        setPokemonAbility(data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.error(err));
   };
 
-  const [berries, setBerries] = useState<BerryType[]>([]);
+  const searchPokemon = (pokemonName: string) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPokemonData(data);
+        getPokemonAbility(data.id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const getBerries = () => {
     fetch("https://pokeapi.co/api/v2/berry")
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        console.log(data.results);
-        setBerries(data.results);
+        setBerryData(data.results);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.error(err));
   };
 
-  const searchPokemon = (pokemonName: string) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-      .then((response) => {
-        return response.json();
+  const getCapitalCity = () => {
+    fetch("https://restcountries.com/v3.1/capital/zagreb")
+      .then((res) => {
+        return res.json();
       })
       .then((data) => {
-        console.log(data);
+        setFlag(data[0].flags.png);
       })
-      .catch((err) => {
-        console.log(err);
-      });
+      .catch((err) => console.error(err));
   };
+
   useEffect(() => {
-    searchPokemon("bulbasaur");
     getBerries();
     getCapitalCity();
   }, []);
 
   return (
     <div className="container">
-      <h1 className="pokemons__title">Search Pokemon Color</h1>
-      <div className="pokemons__header"></div>
-      <h1>Capital City</h1>
-      <div></div>
-      {/* <h1>All berries</h1> */}
-      {/* <div> */}
-      {/* {berries.map((berry) => { */}
-      {/* return <div>{berry.name}</div>; */}
-      {/* })} */}
-      {/* </div> */}
+      <h1 className="pokemons__title">Search Pokemon Colors!</h1>
+      <div className="pokemons__header">
+        <input
+          className="pokemons__input"
+          type="text"
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button
+          className="pokemons__btn"
+          onClick={() => searchPokemon(searchValue)}
+        >
+          Find Pokemon color
+        </button>
+      </div>
+      <div className="pokemons__color-box">
+        {pokemonAbility &&
+          pokemonAbility.names.map((name) => {
+            return <div>{name.name}</div>;
+          })}
+      </div>
+      {/* <h1>All Berries</h1>
+      <div>
+        {berryData.map((berry) => {
+          return (
+            <div>
+              <div>{berry.name}</div>
+              <a href={berry.url}>URL</a>
+            </div>
+          );
+        })}
+      </div> */}
+      <h1>Flag</h1>
+      <img src={flag} alt="A country flag" />
     </div>
   );
 };
