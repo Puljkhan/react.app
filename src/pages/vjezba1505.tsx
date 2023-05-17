98aa83dc82b44d74b4821454a22773f57577ad10
Ivan Puljić <IvanPuljich955@gmail.com>
2023-05-17 17:41:34 +0200

    add playwright

--- src/pages/vjezba1505.tsx
+++ src/pages/vjezba1505.tsx
@@ -3,30 +3,29 @@ import { useEffect } from "react";
 const Vjezba1505 = () => {
   //1. Funkcija koja zbraja dva broja iz parametara
   const add = (a: number, b: number) => {
-    return a + b;
+    return a - b;
   };
   //2. Funkcija koja spaja dva array-a stringova
-  const concatArrays = (a: string, b: string) => {
+  const concatArrays = (a: string[], b: string[]) => {
     return a.concat(b);
   };
 
   //3. Funkcija koja treba u konzolu ispisati brojeve od 1 do 10
   const countToTen = () => {
-    for (var i = 1; i <= 10; i++) {
+    for (let i = 0; i < 11; i++) {
       console.log(i);
     }
   };
 
-  //4. Funkcija koja zbraja sve brojeve iz array-a
-  function sumArray(array: any) {
-    let sum = 0;
+  //4. Funkcija koja zbraja sve broojeve iz array-a
+  const sumArray = (numbers: number[]) => {
+    let sum: number = 0;
 
-    for (let i = 0; i < array.length; i += 1) {
-      sum += array[i];
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
     }
-    console.log(sum);
     return sum;
-  }
+  };
 
   //5. Funkcija koja će vratiti puno ime iz objekta user
   type UserType = {
@@ -42,30 +41,55 @@ const Vjezba1505 = () => {
     return person.firstName + person.lastName;
   };
 
-  getFullName(user);
-
   //6. Funkcija koja pretvara celsius to fahrenheit
   //formula =>  °C * 1.8 + 32 = °F
   const celsiusToFahrenheit = (celsius: number) => {
-    const celsius = prompt(25);
-    return celsius * (1.8 + 32);
+    return celsius * 1.8 + 32;
   };
 
   //7. Funkcija koja ispisuje string naopako
-  const reverseString = (str: boolean) => {
+  const reverseString = (str: string) => {
     let reversedStr = "";
-    for (let i = str.length - 1; i > 0; i--) {
-      reversedStr += str[i - 1];
+    for (let i = str.length - 1; i > -1; i--) {
+      reversedStr += str[i];
     }
     return reversedStr;
   };
 
+  //8.Funkcija kreira palindrom što ne valja?
+  const isPalindrome = (str: string) => {
+    const reversedStr = str.split("").reverse().join("");
+    return str === reversedStr;
+  };
+
+  //9. Funkcija kreira fibonacciev niz ispravi pogresku:
+  const fibonacciSequence = (n: number) => {
+    if (n <= 0) {
+      return [];
+    } else if (n === 1) {
+      return [1];
+    } else if (n === 2) {
+      return [1, 1];
+    } else {
+      let sequence = [1, 1];
+      for (let i = 2; i <= n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+      }
+      return sequence;
+    }
+  };
+
+  //10. Funkcjia filtrira sve parne brojeve iz array-a ispravi gresku
+  const filterEvenNumbers = (arr: number[]) => {
+    return arr.filter((num) => {
+      if (num % 2 === 0) {
+        return num;
+      }
+    });
+  };
+
   useEffect(() => {
     //Ovdje možemo pokretati funkciju koju testiramo
-    console.log(add(1, 2));
-    console.log(concatArrays);
-    countToTen();
-    sumArray([1, 2, 3, 4, 5, 6]);
   }, []);
   return <div className="container"></div>;
 };
