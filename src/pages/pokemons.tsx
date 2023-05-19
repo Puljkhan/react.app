08aed153e89cd98a11a67208c5a0d85ea5f7d3c4
Ivan Puljić <IvanPuljich955@gmail.com>
2023-05-19 15:42:31 +0200

    add new pokemons

--- src/pages/pokemons.tsx
+++ src/pages/pokemons.tsx
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
-
-type PokemonType = {
+type PokemonTypeTypeType = {
   name: string;
-  id: number;
   url: string;
 };
 
+type PokemonTypesType = {
+  slot: number;
+  type: PokemonTypeTypeType[];
+};
+type PokemonType = {
+  id: number;
+  name: string;
+  types: PokemonTypesType[];
+};
 type PokemonAbilityNames = {
   name: string;
 };
-
-type PokemonAbilityType = {
-  name: string;
+type PokemonAbility = {
   id: number;
+  name: string;
   names: PokemonAbilityNames[];
 };
 
@@ -23,28 +29,29 @@ type BerryType = {
 
 const Pokemons = () => {
   const [pokemonData, setPokemonData] = useState<PokemonType>();
-  const [pokemonAbility, setPokemonAbility] = useState<PokemonAbilityType>();
+  const [pokemonAbility, setPokemonAbility] = useState<PokemonAbility>();
   const [searchValue, setSearchValue] = useState<string>("");
-  const [berryData, setBerryData] = useState<BerryType[]>([]);
+  const [berries, setBerries] = useState<BerryType[]>([]);
   const [flag, setFlag] = useState<string>("");
 
   const getPokemonAbility = (id: number) => {
     fetch(`https://pokeapi.co/api/v2/ability/${id}`)
-      .then((res) => {
-        return res.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
         setPokemonAbility(data);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => console.log(err));
   };
 
   const searchPokemon = (pokemonName: string) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
-      .then((res) => {
-        return res.json();
+    fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName.toLocaleLowerCase()}`
+    )
+      .then((response) => {
+        return response.json();
       })
       .then((data) => {
+        console.log(data);
         setPokemonData(data);
         getPokemonAbility(data.id);
       })
@@ -59,20 +66,22 @@ const Pokemons = () => {
         return res.json();
       })
       .then((data) => {
-        setBerryData(data.results);
+        //console.log(data.results);
+        setBerries(data.results);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => console.log(err));
   };
 
   const getCapitalCity = () => {
-    fetch("https://restcountries.com/v3.1/capital/Sofia")
-      .then((res) => {
-        return res.json();
-      })
+    fetch("https://restcountries.com/v3.1/capital/zagreb")
+      .then((res) => res.json())
       .then((data) => {
+        console.log(data[0].flags.png);
         setFlag(data[0].flags.png);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   useEffect(() => {
@@ -82,18 +91,18 @@ const Pokemons = () => {
 
   return (
     <div className="container">
-      <h1 className="pokemons__title">Search Pokemon Colors!</h1>
+      <h1 className="pokemons__title">Search pokemon color</h1>
       <div className="pokemons__header">
         <input
-          className="pokemons__input"
           type="text"
+          className="pokemons__input"
           onChange={(e) => setSearchValue(e.target.value)}
         />
         <button
           className="pokemons__btn"
           onClick={() => searchPokemon(searchValue)}
         >
-          Find Pokemon color
+          Find pokemon color
         </button>
       </div>
       <div className="pokemons__color-box">
@@ -102,19 +111,19 @@ const Pokemons = () => {
             return <div>{name.name}</div>;
           })}
       </div>
-      {/* <h1>All Berries</h1>
-      <div>
-        {berryData.map((berry) => {
+      {/* <h1>All berries</h1> */}
+      {/* <div>
+        {berries.map((berry) => {
           return (
             <div>
               <div>{berry.name}</div>
-              <a href={berry.url}>URL</a>
+              <a href={berry.url}>Link</a>
             </div>
           );
         })}
       </div> */}
       <h1>Flag</h1>
-      <img src={flag} alt="A country flag" />
+      <img src={flag} alt="flag" />
     </div>
   );
 };
