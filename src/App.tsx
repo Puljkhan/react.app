4ca2e85975c8066b752f0a1d7cad4da02c32d151
Ivan Puljić <IvanPuljich955@gmail.com>
2023-06-02 17:14:23 +0200

    add new bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import "./styles/styles.scss";
-import Header from "./components/header";
-import Navigation from "./components/navigation";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Exe from "./pages/exe";
-import Ispit from "./pages/ispit";
-import Home from "./pages/home";
-import Mlinar from "./pages/mlinar";
-import Fave from "./pages/fave";
-import Varijable from "./pages/varijable";
-import Petlje from "./pages/petlje";
-import Vjezba2604 from "./pages/vjezba2604";
-import Vjezba2804 from "./pages/vjezba2804";
-import Vjezba0305 from "./pages/vjezba0305";
+import Header from "./components/header";
+import Bootstrap from "./pages/bootstrap/bootstrap";
 import Breweries from "./pages/breweries";
-import Pokemons from "./pages/pokemons";
 import Europe from "./pages/europe";
 import EuropeCountry from "./pages/europe-country";
-import Countries from "./pages/countries";
-import Chat from "./pages/chat";
-import Vjezba1505 from "./pages/vjezba1505";
+import Exe from "./pages/exe";
+import Home from "./pages/home";
+import Ispit from "./pages/ispit";
+import Mlinar from "./pages/mlinar";
+import Petlje from "./pages/petlje";
+import Pokemons from "./pages/pokemons";
 import Todo from "./pages/todo";
-import Vjezba1905 from "./pages/vjezba1905";
-import Running from "./pages/running";
-import Bootstrap from "./pages/bootstrap";
+import Varijable from "./pages/varijable";
+import Vjezba0305 from "./pages/vjezba0305";
+import Vjezba1505 from "./pages/vjezba1505";
+import Vjezba2604 from "./pages/vjezba2604";
+import Vjezba2804 from "./pages/vjezba2804";
+
+import "./styles/styles.scss";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -31,28 +26,52 @@ const App = () => {
       element: <Home />,
     },
     {
+      path: "/ispit",
+      element: <Ispit />,
+    },
+    {
+      path: "/exe",
+      element: <Exe />,
+    },
+    {
       path: "/mlinar",
       element: <Mlinar />,
+    },
+    {
+      path: "/varijable",
+      element: <Varijable />,
+    },
+    {
+      path: "/petlje",
+      element: <Petlje />,
     },
     {
       path: "/vjezba2604",
       element: <Vjezba2604 />,
     },
     {
-      path: "/chat",
-      element: <Chat />,
+      path: "/vjezba2804",
+      element: <Vjezba2804 />,
     },
     {
-      path: "/running",
-      element: <Running />,
+      path: "/vjezba0305",
+      element: <Vjezba0305 />,
     },
     {
-      path: "/bootstrap",
-      element: <Bootstrap />,
+      path: "/breweries",
+      element: <Breweries />,
     },
     {
       path: "/pokemons",
       element: <Pokemons />,
+    },
+    {
+      path: "/europe",
+      element: <Europe />,
+    },
+    {
+      path: "/europe/:capitalId",
+      element: <EuropeCountry />,
     },
     {
       path: "/vjezba1505",
@@ -63,62 +82,15 @@ const App = () => {
       element: <Todo />,
     },
     {
-      path: "/vjezba1905",
-      element: <Vjezba1905 />,
-    },
-    {
-      path: "/europecountry",
-      element: <EuropeCountry />,
-    },
-    {
-      path: "/countries",
-      element: <Countries />,
-    },
-    {
-      path: "/vjezba0305",
-      element: <Vjezba0305 />,
-    },
-    {
-      path: "/europe",
-      element: <Europe />,
-    },
-    {
-      path: "/breweries",
-      element: <Breweries />,
-    },
-    {
-      path: "/vjezba2804",
-      element: <Vjezba2804 />,
-    },
-    {
-      path: "/exe",
-      element: <Exe />,
-    },
-    {
-      path: "/ispit",
-      element: <Ispit />,
-    },
-    {
-      path: "/petlje",
-      element: <Petlje />,
-    },
-    {
-      path: "/fave",
-      element: <Fave />,
-    },
-    {
-      path: "/varijable",
-      element: <Varijable />,
+      path: "/bootstrap",
+      element: <Bootstrap />,
     },
   ]);
 
   return (
     <div className="App">
       <Header />
-
       <RouterProvider router={router} />
-
-      <Navigation />
     </div>
   );
 };
--- src/pages/vjezba2804.tsx
+++ src/pages/vjezba2804.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
+import { frontendRazred } from "../data/frontendRazred";
+
 type GenderType = "male" | "female";
 
-type StudentType = {
+export type StudentType = {
   name: string;
   lastName: string;
   age: number;
@@ -12,198 +15,17 @@ type StudentType = {
 };
 
 const Vjezba2804 = () => {
-  const frontendRazred: StudentType[] = [
-    {
-      name: "Ivan",
-      lastName: "Sakoman",
-      age: 29,
-      gender: "male",
-      height: 183,
-      hobby: "Cycling",
-      employed: true,
-      favoriteColor: "red",
-    },
-    {
-      name: "Goran",
-      lastName: "Viljanac",
-      age: 19,
-      gender: "male",
-      height: 185,
-      hobby: "Musician",
-      employed: true,
-      favoriteColor: "green",
-    },
-    {
-      name: "Katharina",
-      lastName: "Zlosa",
-      age: 27,
-      gender: "female",
-      height: 180,
-      hobby: "reading",
-      employed: true,
-      favoriteColor: "green",
-    },
-    {
-      name: "Antun",
-      lastName: "Glumac",
-      age: 22,
-      gender: "male",
-      height: 185,
-      hobby: "Football",
-      employed: false,
-      favoriteColor: "Red",
-    },
-    {
-      name: "Nikolina",
-      lastName: "Kirčanski",
-      age: 40,
-      gender: "female",
-      height: 164,
-      hobby: "Martial arts",
-      employed: true,
-      favoriteColor: "black",
-    },
-    {
-      name: "Robert",
-      lastName: "Đujić",
-      age: 24,
-      gender: "male",
-      height: 182,
-      hobby: "Books, movies, games",
-      employed: false,
-      favoriteColor: "silver",
-    },
-    {
-      name: "Monika",
-      lastName: "Ivankovic",
-      age: 23,
-      gender: "female",
-      height: 164,
-      hobby: "roller skating",
-      employed: true,
-      favoriteColor: "grey",
-    },
-    {
-      name: "Maja",
-      lastName: "Juratovac",
-      age: 32,
-      gender: "female",
-      height: 169,
-      hobby: "dancing",
-      employed: true,
-      favoriteColor: "blue",
-    },
-    {
-      name: "Ivan",
-      lastName: "Puljić",
-      age: 25,
-      gender: "male",
-      height: 175,
-      hobby: "running",
-      employed: false,
-      favoriteColor: "blue",
-    },
-    {
-      name: "Sara",
-      lastName: "Cindric",
-      age: 24,
-      gender: "female",
-      height: 161,
-      hobby: "writing",
-      employed: false,
-      favoriteColor: "black",
-    },
-    {
-      name: "Kristian",
-      lastName: "Radoš",
-      age: 30,
-      gender: "male",
-      height: 188,
-      hobby: "Netflix",
-      employed: true,
-      favoriteColor: "blue",
-    },
-    {
-      name: "Ivana",
-      lastName: "Arbutina",
-      age: 42,
-      gender: "female",
-      height: 166,
-      hobby: "slikanje",
-      employed: false,
-      favoriteColor: "red",
-    },
-  ];
-  //Godine
+  //1. Zadatak
+  //Pronađi sve osobe kojima ime počinje na slovo "M"
+  //Pronađi sve osobe koje su vislje od 160 a niže od 180
+  //Pronađi sve osobe kojima je najdraža boja "red"
+  //Pronađi sve osobe kojima prezime završava na "c" ili "ć"
 
-  const numbers = [42, 30, 24, 25, 32, 23, 24, 40, 22, 27, 19, 29];
+  //2. Zadatak
+  //Ispiši u tablicu sve članove razreda
 
-  let result = numbers[0];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] < result) {
-      result = numbers[i];
-    }
-  }
-
-  console.log("Smallest number is: " + result);
-
-  //Imena
-
-  var listNames = [
-    "Ivana",
-    "Ivan",
-    "Kristian",
-    "Sara",
-    "Maja",
-    `Monika`,
-    `Robert`,
-    `Nikolina`,
-    `Antun`,
-    `Katharina`,
-    `Goran`,
-  ];
-
-  listNames.forEach((i) => {
-    if (i.startsWith("M")) {
-      console.log(i);
-    }
-  });
-
-  //Visina
-
-  //Boje
-
-  var listfavoriteColor = [
-    "red",
-    "blue",
-    "black",
-    "blue",
-    "blue",
-    `gray`,
-    `silver`,
-    `black`,
-    `red`,
-    `green`,
-    `red`,
-  ];
-
-  listfavoriteColor.forEach((i) => {
-    if (i.startsWith("red")) {
-      console.log(i);
-    }
-  });
-
-  //Prezime
-
-  let lastName = [];
-
-  for (let i = 0; i < frontendRazred.length; i++) {
-    if (/(c|ć)$/.test(frontendRazred[i].lastName)) {
-      lastName.push(frontendRazred[i].lastName);
-    }
-  }
-
-  console.log("People whose name end in c or ć are: " + lastName);
+  //3. Zadatak
+  //Obojaj tekst "favoriteColor" u boju koja je napisana npr ako je favoriteColor: red, taj tekst u tablici treba biti crvene boje
 
   return (
     <div className="container">
@@ -214,12 +36,7 @@ const Vjezba2804 = () => {
           <thead>
             <tr>
               <th>Name</th>
-              <th>Last Name</th>
-              <th>Age</th>
-              <th>Gender</th>
-              <th>Height</th>
               <th>Hobby</th>
-              <th>Employed</th>
               <th>favoriteColor</th>
             </tr>
           </thead>
@@ -228,35 +45,15 @@ const Vjezba2804 = () => {
               return (
                 <tr>
                   <td>{student.name}</td>
-                  <td>{student.lastName}</td>
-                  <td>{student.age}</td>
-                  <td>{student.gender}</td>
-                  <td>{student.height}</td>
                   <td>{student.hobby}</td>
-                  <td>{student.employed ? "Yes" : "No"}</td>
-                  <td style={{ color: student.favoriteColor }}>
-                    {student.favoriteColor}
-                  </td>
+                  <td>{student.favoriteColor}</td>
                 </tr>
               );
             })}
           </tbody>
         </table>
       </div>
-      <hr />
-      <div>
-        <ul>
-          {frontendRazred.map((student) => {
-            return (
-              <li>
-                {student.name} {student.lastName} {student.age}{" "}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
     </div>
   );
 };
-
 export default Vjezba2804;
